ee213451ee8f866088059b1d0ab75663f4b6b8f6
evkingen <38916658+evkingen@users.noreply.github.com>
2018-11-04 13:48:56 +0300

    Feature ex2 (#1)
    * NewsActivity - добавлены кнопка Send Message, иконки обратной связи.
    activity_news.xml - разметка для потретной ориентации.
    layout-land\activity_news.xml - разметка для альбомной ориентации.
    detail_description.xml - используется в разметках activity_news. Отображает детальную информацию.
    Обновлены файлы с ресурсами.
    
    * Доработка по общим замечаниям.
    В разметках изменены отступы от границ экрана с margin на padding
    Переписан detal_description.xml с RelativeLayout на LinearLayout
    Уменьшены иконки
    Доработана кнопка SEND MESSAGE
    
    * небольшие правки по коду
    
    * небольшая правка по коду
    
    * Удалены большие лишние изображения.
    
    * Коррекция размера изображений.
    
    * Замечание по грэдлу. Убраны лишние зависимости.
    
    * Переименовано название активити. Переименованы переменные. Изменена функция sendMail: intent предлагает на открытие только почтовое предложение.
    
    * Правки по макету:
    форматирование кода,
    переименование переменных,
    изменены значения описаний imageview,
    divider вынесен в отдельный файл.
    
    * email_subject перенесен в строковый ресурс
    
    * добавлен модификатор final к read-only переменным

---
+++ app/src/main/java/com/alohagoha/newsapp/AboutAppActivity.java
@@ -0,0 +1,61 @@
+package com.alohagoha.newsapp;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class AboutAppActivity extends AppCompatActivity {
+
+
+    private static final String INSTAGRAM_URL = "https://www.instagram.com/evkingen";
+    private static final String VK_URL = "https://www.vk.com/id1227900";
+    private static final String TELEGRAM_URL = "https://t.me/alohagoha";
+    private static final String EMAIL_ADDRESS = "zlock14@gmail.com";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_app);
+
+        final EditText feedbackEditText = findViewById(R.id.et_feedback);
+
+        final Button sendButton = findViewById(R.id.btn_send);
+        sendButton.setOnClickListener(view -> {
+            String emailText = feedbackEditText.getText().toString();
+            sendMail(emailText);
+        });
+
+        final ImageView instagramImageView = findViewById(R.id.iv_my_instagram_link);
+        instagramImageView.setOnClickListener(view -> openURL(INSTAGRAM_URL));
+
+        final ImageView vkImageView = findViewById(R.id.iv_my_vk_link);
+        vkImageView.setOnClickListener(view -> openURL(VK_URL));
+
+        final ImageView telegramImageView = findViewById(R.id.iv_my_telegram_link);
+        telegramImageView.setOnClickListener(view -> openURL(TELEGRAM_URL));
+
+    }
+
+    private void openURL(String url) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+        startActivity(intent);
+    }
+
+    private void sendMail(String emailText) {
+        final Intent intent = new Intent(Intent.ACTION_SENDTO)
+                .setData(Uri.parse(String.format("mailto:%s", EMAIL_ADDRESS)))
+                .putExtra(Intent.EXTRA_SUBJECT, R.string.feedback_email_subject)
+                .putExtra(Intent.EXTRA_TEXT, emailText);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, R.string.error_no_email_app, Toast.LENGTH_LONG).show();
+        }
+    }
+}
--- app/src/main/java/com/alohagoha/newsapp/NewsActivity.java
+++ app/src/main/java/com/alohagoha/newsapp/NewsActivity.java
@@ -1,13 +1,57 @@
 package com.alohagoha.newsapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class NewsActivity extends AppCompatActivity {
+
+    private static final String INSTAGRAM_URL = "https://www.instagram.com/evkingen";
+    private static final String VK_URL = "https://www.vk.com/id1227900";
+    private static final String TELEGRAM_URL = "https://t.me/alohagoha";
+    private static final String EMAIL_ADDRESS = "zlock14@gmail.com";
+    private static final String EMAIL_SUBJECT = "Feedback";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news);
+        final EditText et = findViewById(R.id.feedback_et);
+        Button btn_send = findViewById(R.id.send_btn);
+        btn_send.setOnClickListener(view -> {
+            String emailText = et.getText().toString();
+            sendMail(emailText);
+        });
+
+        ImageView iv_ref_1 = findViewById(R.id.iv_ref_1);
+        iv_ref_1.setOnClickListener(view -> openURL(INSTAGRAM_URL));
+
+        ImageView iv_ref_2 = findViewById(R.id.iv_ref_2);
+        iv_ref_2.setOnClickListener(view -> openURL(VK_URL));
+
+        ImageView iv_ref_3 = findViewById(R.id.iv_ref_3);
+        iv_ref_3.setOnClickListener(view -> openURL(TELEGRAM_URL));
+
+    }
+
+    private void openURL(String URL) {
+        Uri uri = Uri.parse(URL);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        startActivity(intent);
+    }
+
+    private void sendMail(String emailText) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/html");
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] {EMAIL_ADDRESS});
+        intent.putExtra(Intent.EXTRA_SUBJECT,EMAIL_SUBJECT);
+        intent.putExtra(Intent.EXTRA_TEXT,"Text1 " + emailText);
+        startActivity(intent);
     }
 }
